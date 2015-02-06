27120e2fc95b17e671f93c4d318e99a17838ce0b
Peter L-G <peter.larsson.green@gmail.com>
2015-02-06 16:14:01 +0100

    Removed unneccessary comment.

--- package.js
+++ package.js
@@ -2,7 +2,6 @@ Package.describe({
 	name: 'peppelg:mongo-sum',
 	version: '1.0.0',
 	summary: 'Reactive sum queries in MiniMongo on client.',
-	// URL to the Git repository containing the source code for this package.
 	git: 'https://github.com/PeppeL-G/mongo-sum.git',
 	documentation: 'README.md'
 })
